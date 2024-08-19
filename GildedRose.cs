2215c05161a385db91064244f8a1b596f7339cde
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-19 13:17:10 +0100

    ♻️ Invert and combinated several if else un one.

--- GildedRose.cs
+++ GildedRose.cs
@@ -18,23 +18,16 @@ namespace GildedRoseKata
             {
                 if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros") item.Quality = item.Quality - 1;
-                    }
+                    if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros") item.Quality = item.Quality - 1;
                 }
                 else if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
+                {
+                    item.Quality = item.Quality + 1;
 
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11) item.Quality = item.Quality + 1;
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 11) item.Quality = item.Quality + 1;
 
-
-                            if (item.SellIn < 6) item.Quality = item.Quality + 1;
-                        }
-                    }
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 6) item.Quality = item.Quality + 1;
+                }
 
                 if (item.Name != "Sulfuras, Hand of Ragnaros") item.SellIn = item.SellIn - 1;
 
@@ -42,14 +35,8 @@ namespace GildedRoseKata
                 {
                     if (item.Name != "Aged Brie")
                     {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                        else item.Quality = 0;
+                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert") item.Quality = 0;
+                        if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros") item.Quality = item.Quality - 1;
                     }
                     else if (item.Quality < 50) item.Quality = item.Quality + 1;
                 }
